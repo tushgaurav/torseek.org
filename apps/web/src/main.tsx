55df4e228395a7d8300df576e5a710f77bf7eed6
Tushar Gaurav <tushar.gaurav416@gmail.com>
2026-09-14 21:05:20 +0530

    chore: add posthog

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,18 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import { ThemeProvider } from 'next-themes'
+import { PostHogProvider } from '@posthog/react'
 
 import { TooltipProvider } from '@/components/ui/tooltip'
 import App from './App.tsx'
 import './index.css'
 
+const options = {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+  defaults: '2026-05-30'
+} as const
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
       <TooltipProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <PostHogProvider
+          apiKey={import.meta.env.VITE_POSTHOG_PROJECT_TOKEN}
+          options={options}
+        >
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PostHogProvider>
       </TooltipProvider>
     </ThemeProvider>
   </StrictMode>,
